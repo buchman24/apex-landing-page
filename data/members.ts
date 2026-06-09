bcec75cb0af93bc9fd36c9262dd3c0c98a0ce069
buchman24 <buchman@apex.org.il>
2026-06-09 16:42:10 +0300

    Add community member: Omri Sela

--- data/members.ts
+++ data/members.ts
@@ -88,6 +88,7 @@ export const communityMembers: Member[] = [
   { id: "tamir_m", name: "Tamir Meerovitch", description: "Sr. Growth Strategy Executive at Frost & Sullivan", imageSrc: "/team/tal_meerovitch.jpeg", linkedinUrl: "https://www.linkedin.com/in/tamir-meerovitch/" },
   { id: "itzik_p", name: "Itzik Polad", description: "Chief Data Officer at LSports", imageSrc: "/team/itzik.webp", linkedinUrl: "https://www.linkedin.com/in/itzik-polad/" },
   { id: "ido_ben_david", name: "Ido Ben David", description: "Technology Partners Manager at Nebius", imageSrc: "/team/ido_ben_david.jpg", linkedinUrl: "https://www.linkedin.com/in/idobendavid" },
+  { id: "omri_sela", name: "Omri Sela", description: "Head of GTM - Plurai", imageSrc: "/team/omri_sela.jpg", linkedinUrl: "https://www.linkedin.com/in/omri-s/" },
 ];
 
 export const allMembers: Member[] = [...foundingTeamMembers, ...communityMembers];
